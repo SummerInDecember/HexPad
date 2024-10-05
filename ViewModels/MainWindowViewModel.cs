91a26819d2326cb3028667a7e1772384a32502e0
SUmmerInDecember <gabe_fish@protonmail.com>
2024-10-06 00:02:01 +0100

    started working

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Reactive;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
+using HexPad.Views;
+using ReactiveUI;
+
+namespace HexPad.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+#pragma warning disable CA1822 // Mark members as static
+
+    public string SelectedFolder { get; set; } = String.Empty;
+    public string SelectedFile { get; set; } = String.Empty;  
+    public MainWindowViewModel()
+    {
+    }
+
+
+    
+#pragma warning restore CA1822 // Mark members as static
+
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,68 @@
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using HexPad.ViewModels;
+using System;
+using System;
+using System.Linq;
+using System.Reactive;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
+using HexPad.Views;
+using ReactiveUI;
+
+namespace HexPad.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = new MainWindowViewModel();
+    }
+
+    private async void OpenFolderButton_Click(object sender, RoutedEventArgs e)
+    {
+        await OpenFolder();
+    }
+    private async Task OpenFolder()
+    {
+        MainWindowViewModel viewModel = (MainWindowViewModel)DataContext;
+        var folder = await this.StorageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions
+        {
+            Title = "Select A Folder",
+            AllowMultiple = false
+        });
+
+        if (folder.Any())
+            viewModel.SelectedFolder = folder.First().Path.LocalPath;
+        else
+            viewModel.SelectedFolder = String.Empty;
+
+
+        Console.WriteLine(viewModel.SelectedFolder);
+    }
+
+    private async void OpenFile_Click(object sender, RoutedEventArgs e)
+    {
+        await OpenFile();
+    }
+
+    private async Task OpenFile()
+    {
+        MainWindowViewModel viewModel = (MainWindowViewModel)DataContext;
+        var file = await this.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = "Select a File",
+            AllowMultiple = false
+        });
+
+        if(file.Any()) 
+            viewModel.SelectedFile = file.First().Name; 
+        else    
+            viewModel.SelectedFile = String.Empty;
+
+        Console.WriteLine(viewModel.SelectedFile);
+    }
+}
